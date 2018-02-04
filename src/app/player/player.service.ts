708542d5ef3a0a3cb2913979b99e46e410cd4db1
Ted Robinson <tfqbtd@aol.com>
2018-02-04 00:37:35 -0600

    updated first song

--- src/app/player/player.service.ts
+++ src/app/player/player.service.ts
@@ -6,7 +6,7 @@ export class PlayerService {
     playerSelected = new EventEmitter<Player>();
 
     private playerlists: Player[] = [
-        new Player('eOkaj6GBBIo', 'Only Love Can  Break Your Heart'),
+        new Player('LgPpowVNEfE', 'Musiq - So beautiful'),
         new Player('450p7goxZqg', 'All of me'),
         new Player('g5Z904AccvM', 'I bring me'),
         new Player('PMivT7MJ41M', 'That"s What I Like'),
