ce791ab4e9f263b31c2efe208da7b5f62aea15f9
Ted Robinson <tfqbtd@aol.com>
2018-02-04 15:49:03 -0600

    Added home page

--- src/app/player/player.service.ts
+++ src/app/player/player.service.ts
@@ -9,7 +9,7 @@ export class PlayerService {
         new Player('LgPpowVNEfE', 'Musiq - So beautiful'),
         new Player('450p7goxZqg', 'All of me'),
         new Player('g5Z904AccvM', 'I bring me'),
-        new Player('PMivT7MJ41M', 'That"s What I Like'),
+        new Player('PMivT7MJ41M', 'That`s What I Like'),
         new Player('oOfGGjr6Aq4', 'Share My Life'),
         new Player('62iOcrKHG6Q', 'River'),
         new Player('XxGh4ruG33M', 'FourPlay')
