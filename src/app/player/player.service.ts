c589ef96cdc7cbbedc77b3ffae04307853e66c93
Ted Robinson <tfqbtd@aol.com>
2018-02-04 01:51:45 -0600

    update the about page

--- src/app/player/player.service.ts
+++ src/app/player/player.service.ts
@@ -12,7 +12,7 @@ export class PlayerService {
         new Player('PMivT7MJ41M', 'That"s What I Like'),
         new Player('oOfGGjr6Aq4', 'Share My Life'),
         new Player('62iOcrKHG6Q', 'River'),
-        new Player('GwP8xAh20FU', 'Stay Awake')
+        new Player('XxGh4ruG33M', 'FourPlay')
       ];
 
       getPlayers() {
