4dc77a3193c95aa49a2d9136879dda03f3cbfeed
Ted Robinson <tfqbtd@aol.com>
2018-05-11 11:39:26 -0500

    update playlist

--- src/app/player/player.service.ts
+++ src/app/player/player.service.ts
@@ -10,13 +10,17 @@ export class PlayerService {
     private playerlists: Player[] = [
         new Player('LgPpowVNEfE', 'Musiq - So beautiful'),
         new Player('450p7goxZqg', 'All of me'),
+        new Player('6YNZlXfW6Ho', 'Ella mai - Boo`d up'),
         new Player('g5Z904AccvM', 'I bring me'),
+        new Player('H64QG4UsrGI', 'Mario - let me Love You'),
         new Player('PMivT7MJ41M', 'That`s What I Like'),
         new Player('oOfGGjr6Aq4', 'Share My Life'),
         new Player('62iOcrKHG6Q', 'River'),
+        new Player('HhuGQUZJot8', 'Alicia Keys - Un-thinkable'),
         new Player('XxGh4ruG33M', 'Fourplay-I`ll Still Be Loving You'),
         new Player('jT0gbcq5xqI', 'You and I'),
         new Player('seZMOTGCDag', 'Jack Johnson - Better Togeher'),
+        new Player('xpVfcZ0ZcFM', 'Drake - God`s Plan'),
         new Player('EtJy69cEOtQ', 'How to Learn Anything'),
         new Player('BxY_eJLBflk', 'Put God First - Denzel Washington'),
         // new Player('3B87LMXvUsM', 'Black Panther'), lFsChbwsbmo 'Insanity Gregory Porter", 5oSnLt20Wn4 'Consequence of Love'
@@ -28,7 +32,7 @@ export class PlayerService {
         new Player('Qn25lL4a94o', 'Dr. SaxLove, Max Maxwell'),
         new Player('lFsChbwsbmo', 'Insanity Gregory Porter'),
         new Player('5oSnLt20Wn4', 'Consequence of Love'),
-        new Player('TvnYmWpD_T8', 'Prince-Purple Rain'),
+        new Player('zDj34uJdH40', 'Selena Gomez & Justin Bieber'),
         new Player('n2v98PGBZH4', 'Billy Paul-Me And Mrs.Jones'),
         // new Player('_8o71Hlzonk', 'Stay With Me, Sam Smith'),
         new Player('QUmxh7H8vok', 'Anthony Hamilton - Charlene')
